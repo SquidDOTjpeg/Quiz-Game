32f42056d2e93a02899c5981897ab6a9fba514a5
AnthonyPillow <anthonypillow18@gmail.com>
2020-01-28 00:28:30 -0800

    Moved on with more js. Getting the Start function to work properly. Not sure if I'm going to do the score or timer next.

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ var score
 var startBtn = document.getElementById("start-btn")
 var answerBtn = document.getElementById("answer-btn")
 var vhsBtn = document.getElementById("high-score-btn")
+var questionText = document.getElementById("question-text")
+
 
 function start(){
     startBtn.classList.add("hide")
     vhsBtn.classList.add("hide")
+    answerBtn.classList.remove("hide")
+    questionText.classList.remove("hide")
 }
