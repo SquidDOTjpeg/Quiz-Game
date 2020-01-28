ebd0c4b1856bbf78becf825d5e23dc0eabbf38bf
AnthonyPillow <anthonypillow18@gmail.com>
2020-01-28 10:19:25 -0800

    Added an object with arrays and nested objects in the js file. Don't know how to link it from a seperate file yet.

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ var answerBtn = document.getElementById("answer-btn")
 var vhsBtn = document.getElementById("high-score-btn")
 var questionText = document.getElementById("question-text")
 
+var questions = [
+    {
+        title: "What language do you use to code the basic frame work of a website?",
+        choices: ["Mandarin", "English", "Hyper Text Markup Language", "I eat crayons"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: "What 2 + 2?",
+        choices: ["4", "22", "8", "I eat crayons"],
+        answer: "4"
+    },
+    {
+        title: "How many questions is the minimum for this assignment?",
+        choices: ["4", "22", "5", "I eat crayons"],
+        answer: "5"
+    },
+    {
+        title: "Hey, you You're finally awake. You were trying to cross the border right?",
+        choices: ["Yes", "No", "Alexa, play Skyrim on my Samsung Smart Fridge", "Look silently into his eyes"],
+        answer: "Look silently into his eyes"
+    },
+    {
+        title: "How many questions is the minimum for this assignment?",
+        choices: ["4", "22", "5", "I eat crayons"],
+        answer: "5"
+    }
+
+]
+
 
 function start(){
     startBtn.classList.add("hide")
