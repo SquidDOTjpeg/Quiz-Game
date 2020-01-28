1b1426288c8bd2a293f097d9f186fb866c00ee66
AnthonyPillow <anthonypillow18@gmail.com>
2020-01-27 21:28:16 -0800

    Decided to restart the entire project and find a video to help me with the process. I am going to code with the video and then go back and customize it further and add a timer.

---
+++ questions.js
@@ -0,0 +1,22 @@
+var questionArray = [
+    {
+        prompt: 'What is he doing here?\n(a) Leaving\n(b) Going',
+        answer: "a"
+    }
+    {
+        prompt: 'What is you doing here?\n(a) Leaving\n(b) Going',
+        answer: "b"
+    }
+    {
+        prompt: 'What is we doing here?\n(a) Leaving\n(b) Going',
+        answer: "b"
+    }
+    {
+        prompt: 'What is Frank, your long time best-friend and confidant, doing here?\n(a) Leaving\n(b) Going',
+        answer: "b"
+    }
+    {
+        prompt: 'What is doing here?\n(a) Leaving\n(b) Going',
+        answer: "b"
+    }
+]
